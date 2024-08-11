206cacd74378ded8565747e5412062b57678db3f
ayush2912 <76970440+ayush2912@users.noreply.github.com>
2024-08-11 16:31:15 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -142,18 +142,13 @@ resource "aws_eks_node_group" "my_nodes" {
     aws_iam_role_policy_attachment.my_nodes-AmazonEC2ContainerRegistryReadOnly,
   ]
 }
-resource "aws_eks_addon" "coredns" {
-  cluster_name = aws_eks_cluster.my_cluster.name
-  addon_name   = "coredns"
-  addon_version = "v1.11.1-eksbuild.9"
-  resolve_conflicts_on_update = "PRESERVE"
-}
 
-resource "aws_eks_addon" "kube_proxy" {
-  cluster_name = aws_eks_cluster.my_cluster.name
-  addon_name   = "kube-proxy"
-  addon_version = "v1.30.0-eksbuild.3"
-}
+
+#resource "aws_eks_addon" "kube_proxy" {
+ # cluster_name = aws_eks_cluster.my_cluster.name
+ # addon_name   = "kube-proxy"
+ # addon_version = "v1.30.0-eksbuild.3"
+#}
 
 resource "aws_eks_addon" "vpc_cni" {
   cluster_name = aws_eks_cluster.my_cluster.name
